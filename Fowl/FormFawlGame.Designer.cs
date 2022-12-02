36a8bba8a3ab615bffb5b8e0897ee4dc47d9c72d
Kiril Spasov <spasov.kiril@gmail.com>
2022-12-02 17:24:07 +0200

    Add project files.

---
+++ Fowl/FormFawlGame.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Fowl
+{
+    partial class FormFawlGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCheck = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnCheck
+            // 
+            this.BtnCheck.Location = new System.Drawing.Point(40, 58);
+            this.BtnCheck.Name = "BtnCheck";
+            this.BtnCheck.Size = new System.Drawing.Size(166, 59);
+            this.BtnCheck.TabIndex = 0;
+            this.BtnCheck.Text = "Check Results";
+            this.BtnCheck.UseVisualStyleBackColor = true;
+            this.BtnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(258, 58);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(478, 348);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormFawlGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnCheck);
+            this.Name = "FormFawlGame";
+            this.Text = "Fawl Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCheck;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ Fowl/FormFawlGame.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Fowl
+{
+    public partial class FormFawlGame : Form
+    {
+        public FormFawlGame()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            string line = "";
+
+            string path = Application.StartupPath + @"\fowl.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            int lineCount = Convert.ToInt32(streamReader.ReadLine());
+
+            for (int i = 1; i <= lineCount; i++)
+            {
+                line = streamReader.ReadLine();
+                TxtResult.Text += "#" + i + " - " + CheckResult(line) + Environment.NewLine;
+            }     
+        }
+
+        private string CheckResult(string input)
+        {
+
+            string result = "";
+
+            int a, b;
+            int c = 0;
+
+            string[] dist = input.Split(' ');
+            int[] distances = new int[dist.Length];
+
+            for (int i = 0; i < distances.Length; i++)
+            {
+                distances[i] = Convert.ToInt32(dist[i]);
+            }
+
+            //We sort them, because numbers are in any order.
+            //First number in the array is the biggest(the hipotenuse).
+            Sort(distances);
+
+            c = distances[0];
+            a = distances[1];
+            b = distances[2];
+
+            //We check if c*c = a*a + b*b.
+            return result = CheckFormula(a, b, c) ? "YES" : "NO"; 
+        }
+
+        private void Sort(int[] distances)
+        {
+            int temp = 0;
+            bool swap = false;
+
+            do
+            {
+                swap = false;
+
+                for (int i = 0; i < distances.Length - 1; i++)
+                {
+                    if (distances[i] < distances[i + 1])
+                    {
+                        temp = distances[i];
+                        distances[i] = distances[i + 1];
+                        distances[i + 1] = temp;
+                        swap = true;
+                    }
+                }
+            }
+            while (swap == true);
+        }
+
+        private bool CheckFormula(int a, int b, int c)
+        {
+            if (Math.Pow(c, 2) == Math.Pow(a, 2) + Math.Pow(b, 2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
